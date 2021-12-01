6bf6cef6bc619eac112d3acec012a137d16f5387
Jhans830 <jhans830@gmail.com>
2021-11-30 19:01:26 -0600

    commit

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -6,9 +6,7 @@ define([
     'use strict';
 
     var connection = new Postmonger.Session();
-    var authTokens = {};
     var payload = {};
-    var eventDefinitionKey;
 
     connection.on('initActivity', initialize);
     connection.on('clickedNext', save);
@@ -19,7 +17,6 @@ define([
             var setcpURL = payload['arguments'].execute.inArguments[0].cloudpageURL;
             $('#cpURL').val(setcpURL);
         }
-
         connection.trigger('updateButton', {
             button: 'next',
             text: 'done',
@@ -32,11 +29,9 @@ define([
         // grab TSD customer key from activity on save
         var cpURL = $('#cpURL').val();
         payload['arguments'].execute.inArguments = [{
-            "subscriberKey": "{{Event." + eventDefinitionKey + ".\"SubscriberKey\"}}",
             "cloudpageURL": cpURL
         }];
         payload['metaData'].isConfigured = true;
-        console.log(payload);
         connection.trigger('updateActivity', payload);
     }
 });
